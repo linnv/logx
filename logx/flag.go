4210af394e8eebdb62988c63bd9d40b028c5ccc1
JialinWu <win27v@gmail.com>
2017-02-06 12:38:28 +0800

    rename model to logx

---
+++ logx/flag.go
@@ -0,0 +1,32 @@
+// Package  provides ...
+package model
+
+import "flag"
+
+var initFlag bool
+
+//InitFlags provides configure parameters of logx by args, you should call this functions after all of other flags have been defined
+func InitFlags() {
+	flag.String("logxfile", "", "absolut path of file,if empty no log will go into file")
+	initFlag = true
+}
+
+//@TODO maybe not only one configure parameter through flag
+func GetFlags() string {
+	if !initFlag {
+		return ""
+	}
+	if flag.Parsed() {
+		var file string
+		visitor := func(f *flag.Flag) {
+			if f.Name == "logxfile" {
+				file = f.Value.String()
+			}
+		}
+		flag.Visit(visitor)
+		return file
+
+	}
+	flag.Parse()
+	return GetFlags()
+}
